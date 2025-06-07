7469f22e49dc4f205ed80bf04a2917bd792028c3
danskie09 <banaybanaydaniel09@gmail.com>
2025-06-07 09:46:25 +0800

    Refactor Tailwind CSS configuration: move inline configuration to a separate partial for improved organization and maintainability.

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -7,45 +7,7 @@
     <title>Dashboard - Rose Tailoring</title>
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=poppins:300,400,500,600,700&family=playfair-display:400,600,700" rel="stylesheet" />
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script>
-        tailwind.config = {
-            theme: {
-                extend: {
-                    colors: {
-                        peach: {
-                            50: '#fef7f0',
-                            100: '#feeee1',
-                            200: '#fddcc2',
-                            300: '#fcca94',
-                            400: '#fab866',
-                            500: '#f8a647',
-                            600: '#f59e42',
-                            700: '#f2963d',
-                            800: '#ef8e38',
-                            900: '#ec8633'
-                        },
-                        rose: {
-                            50: '#fdf2f8',
-                            100: '#fce7f3',
-                            200: '#fbcfe8',
-                            300: '#f9a8d4',
-                            400: '#f472b6',
-                            500: '#ec4899',
-                            600: '#db2777',
-                            700: '#be185d',
-                            800: '#9d174d',
-                            900: '#831843'
-                        }
-                    },
-                    fontFamily: {
-                        sans: ['Poppins', 'sans-serif'],
-                        serif: ['Playfair Display', 'serif']
-                    }
-                }
-            }
-        }
-    </script>
+    @include('app.partials.tailwind-config')
 </head>
 
 <body class="bg-gray-50 font-sans">
